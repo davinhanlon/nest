07203d37ff55bd9f42697498bb6dcaeb176357ba
Davin Hanlon <davin_hanlon@hotmail.com>
2017-12-27 17:01:33 +0000

    add get and set files

--- vars.rb
+++ vars.rb
@@ -1,5 +1,5 @@
-NEST_BASE_URL = 'nest_base_url'
-STAT_ID = 'nest_stat_id'
-AUTH_ID = 'nest_auth_id'
-NEST_STAT_SPECIFIC_URL = 'nest_stat_specific_url'
-TARGET_TEMP = 25
+NEST_BASE_URL = 'https://firebase-apiserver20-tah01-iad01.dapi.production.nest.com:9553'
+STAT_ID = 'wRFYwmAT7rFQ1auMNYlpNzr2E_7Duxt8'
+AUTH_ID = 'Bearer c.p5RAFUUnFqmAaqypUqblXzlfxi1Hlov1bAglvFm6Lbm6mrEJJq2Eiy81DcAwlnFDKULKxmghxmUvyJNvjedNAlcRGBBlGMPxfc3TOkmeY38LHq4cQJGTW0ebFVADnoHskkfjw1sTTxGEapfa'
+NEST_STAT_SPECIFIC_URL = 'https://firebase-apiserver20-tah01-iad01.dapi.production.nest.com:9553/devices/thermostats/wRFYwmAT7rFQ1auMNYlpNzr2E_7Duxt8'
+TARGET_TEMP = 22
